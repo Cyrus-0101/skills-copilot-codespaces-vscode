0d5df60b6275ff81596fa0094bea393ab3f60e82
Cyrus Ndirangu <cyrus_g@outlook.com>
2023-08-14 08:20:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,18 @@
 // Create a web server
 // Load the express module
-const express = require('express');
+import express from 'express';
 const app = express();
 // Load the comments.json file
-const comments = require('./comments.json');
+import comments, { push } from './comments.json';
 // Load the body-parser module
-const bodyParser = require('body-parser');
+import { urlencoded, json } from 'body-parser';
 // Load the fs module
-const fs = require('fs');
+import { writeFile } from 'fs';
 // Set the port number
 const port = 3000;
 // Set up the middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 // Set up the CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -27,12 +27,12 @@ app.post('/comments', (req, res) => {
     // Get the comment from the request body
     const comment = req.body;
     // Add the comment to the "database"
-    comments.push(comment);
+    push(comment);
     // Send back the created comment
     res.json(comment);
     // Write the updated "database" to the comments.json file
     const commentsJSON = JSON.stringify(comments, null, 2);
-    fs.writeFile('comments.json', commentsJSON, err => {
+    writeFile('comments.json', commentsJSON, err => {
         if (err) throw err;
         console.log('The data has been written to the file');
     });
